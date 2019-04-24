b3e99f141bf04674c31c8723da7d0e08ec4b5242
uqjwen <wen_jiahui@outlook.com>
2019-04-25 07:27:37 +0800

    changes

--- model_tf.py
+++ model_tf.py
@@ -38,7 +38,13 @@ class Model():
 		self.dropout = drop_out
 		self.batch_size = batch_size
 		print("embedding size", domain_emb.shape)
+		self.x = tf.placeholder(tf.int32, shape=[None, maxlen])
+		self.labels = tf.placeholder(tf.int32, shape=[None, maxlen, num_class])
 		self.word_embedding = tf.Variable(domain_emb.astype(np.float32))
+		# self.word_embedding = tf.Variable(tf.random_uniform([self.vocab_size, self.emb_size], -1.0,1.0))
+
+		self.x_emb = tf.nn.embedding_lookup(self.word_embedding, self.x)
+
 
 		# self.conv1 = Conv1D(128, kernel_size = 3, padding = 'same')
 		# self.conv2 = Conv1D(128, kernel_size = 5, padding = 'same')
@@ -48,14 +54,16 @@ class Model():
 		# self.conv3 = Conv1D(256, kernel_size = 5, padding='same')
 		# self.conv4 = Conv1D(256, kernel_size = 5, padding = 'same')
 		# self.conv5 = Conv1D(256, kernel_size = 5, padding='same')
+		x_logit = Dense(50, activation='relu', kernel_initializer = 'lecun_uniform')(self.x_emb)
+		x_logit = Dense(3, kernel_initializer='lecun_uniform')(x_logit)
+		# self.linear_ae1 = Dense(50, activation='relu', kernel_initializer = 'lecun_uniform')
 
-		self.linear_ae1 = Dense(50)
+		# self.linear_ae2 = Dense(3, kernel_initializer='lecun_uniform')
 
-		self.linear_ae2 = Dense(3)
+		# self.logits, self.loss = self.forward(num_class)
+		loss = tf.nn.softmax_cross_entropy_with_logits(logits = x_logit, labels = self.labels)
 
-		self.logits, self.loss = self.forward(num_class)
-
-		self.cost = tf.reduce_mean(self.loss)
+		self.cost = tf.reduce_mean(loss)
 
 		self.train_op = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(self.cost)
 
@@ -115,8 +123,8 @@ def train():
 			for b in range(num_batch+1):
 				input_data, mask_data, y_data = data_loader.__next__()
 				# print(input_data.shape, mask_data.shape, y_data.shape)
-				y_data = to_categorical(y_data, 3).reshape(-1,3)
-				print(y_data.shape,'uqjwen')
+				y_data = to_categorical(y_data, 3)
+				# print(y_data.shape,'uqjwen')
 				_,loss = sess.run([model.train_op, model.cost], feed_dict = {model.x:input_data, model.labels:y_data})
 
 				sys.stdout.write('\repoch:{}, batch:{}, loss:{}'.format(i,b,loss))
