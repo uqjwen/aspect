de237a6a0e6f507e0e0509d81866cf20a846abc2
uqjwen <wen_jiahui@outlook.com>
2019-05-28 09:15:37 +0800

    ks

--- model_tf.py
+++ model_tf.py
@@ -378,7 +378,8 @@ def train():
 	val_score 	= []
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
-		saver = tf.train.Saver(tf.global_variables())
+		# saver = tf.train.Saver(tf.global_variables())
+		saver = tf.train.Saver(max_to_keep=10)
 
 
 		ckpt = tf.train.get_checkpoint_state(checkpointer_dir)
