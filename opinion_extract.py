26ca13eeef23d6dd44635ee1852642aa3bce2f04
uqjwen <wen_jiahui@outlook.com>
2019-06-22 11:57:30 +0800

    changes

--- opinion_extract.py
+++ opinion_extract.py
@@ -1,7 +1,10 @@
 from nltk.parse.stanford import StanfordDependencyParser
 import pickle
 import sys
+from collections import Counter
 # filename = './pkl/'
+from nltk.corpus import stopwords
+
 data_file = './pkl/data_laptop_2014.pkl'
 
 def get_parser(domain):
@@ -37,6 +40,80 @@ def get_parser(domain):
 	fr = open(save_filename,'wb')
 	pickle.dump(new_data,fr)
 	fr.close()
+def get_pos(filename):
+	fr = open(filename, 'rb')
+	data = pickle.load(fr)
+	fr.close()
+	return data['tags']
+
+
+def extraction(sents, deps, tags):
+	curr_opinion = ['good','high','easy','great']
+
+
+def opinion(domain):
+	if domain == 'laptop':
+		suffix = '_2014.pkl'
+	else:
+		suffix = '_2016_2.pkl'
+
+	pos_file = './pkl/data_'+domain+suffix
+	dep_file = './pkl/dep_'+domain+suffix
+
+	tags = get_pos(pos_file)
+
+	fr = open(dep_file,'rb')
+	data = pickle.load(fr)
+	fr.close()
+	sents = data['raw_sentence']
+	deps = data['dependency']
+	labels = data['labels']
+
+
+
+
+
+
+	for sent,tag, dep in zip(sents, tags, deps):
+		# print(sent)
+		# print(tag)
+		# print(len(sent), len(tag))
+		# print('-----------------------------')
+		# break
+		print(' '.join(sent))
+		# print(dep)
+		for d in dep:
+			print(d)
+		print('--------------------------------')
+		assert len(sent) == len(tag)
+
+
+
+def frequency_counter(domain):
+	if domain == 'laptop':
+		suffix = '_2014.pkl'
+	else:
+		suffix = '_2016_2.pkl'
+	data_file = './pkl/data_'+domain+suffix
+	fr = open(data_file,'rb')
+	data = pickle.load(fr)
+	fr.close()
+	sents = data['raw_sentence']
+
+	word_c = Counter()
+	for sent in sents:
+		word_c += Counter(sent)
+
+	stop_words = list(set(stopwords.words('english')))
+	print (stop_words)
+
+	common_words = word_c.most_common(200)
+	for word, count in common_words:
+		if word not in stop_words:
+			print(word, count)
+	# print(word_c.most_common(10))
 
 if __name__ == '__main__':
-	get_parser(sys.argv[1])
+	# get_parser(sys.argv[1])
+	# opinion('laptop')
+	frequency_counter('laptop')
